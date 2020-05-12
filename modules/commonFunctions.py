1ee1994407e2731d379187e91d9c70a00718cf92
mja00 <me@mja00.dev>
2020-05-12 15:51:55 -0400

    Swapped back to kraken because fuck Twitch

--- modules/commonFunctions.py
+++ modules/commonFunctions.py
@@ -5,9 +5,9 @@ def channelIDToName(channelID, clientID):
     headers = {
         'Client-ID': clientID
     }
-    r = requests.get(f"https://api.twitch.tv/helix/users?id={channelID}", headers=headers)
+    r = requests.get(f"https://api.twitch.tv/kraken/users/{channelID}", headers=headers)
     jsonObj = r.json()
-    return jsonObj["data"][0]["login"]
+    return jsonObj["data"][0]["display_name"]
 
 def isUserVerified(mongoDBObj, user):
     if mongoDBObj.find({'discord': str(user.id)}).count() > 0:
